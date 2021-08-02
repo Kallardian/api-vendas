6f3a2d5b7fc723320bac6e7acbdc384d9b1d2ce7
Kallardian <paulobertanha03@gmail.com>
2021-08-02 19:50:23 -0300

    Criando entidade Products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
-import '../typeorm';
+import '../typeorm/index.ts';
 
 const app = express();
 
